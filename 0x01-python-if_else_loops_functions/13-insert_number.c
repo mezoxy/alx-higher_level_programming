ff522e7ac2712ad1ca9ccb7ec07dc063fc263d50
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-05 15:38:40 -0700

    Task 8

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+
+}
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        if ord(str[i]) < 123 and ord(str[i]) > 96:
-            print("{}".format(chr(ord(str[i])) - 32), end="" if i < len(str) - 1 else "\n")
-        else: 
+
+        if ord(str[i]) in range(97, 123):
+            x = chr(ord(str[i]) - 32)
+            print("{}".format(x), end="" if i < len(str) - 1 else "\n")
+        else:
             print("{}".format(str[i]), end="" if i < len(str) - 1 else "\n")
