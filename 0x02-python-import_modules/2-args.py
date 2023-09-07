b2f63175fc3e3c1b64448b61986fc5fa2444c458
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-07 11:43:17 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
     else:
-        print("{} {}".format("0", "arguments."))
+        print("{} {}".format(0, "arguments."))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print("{}".format(0))
