47f3a1fabfc35ddb27feb6cd8dca2b0dcb07161e
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-07 11:16:50 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     if len(sys.argv) != 1:
         print("{} {}".format(len(sys.argv) - 1, "arguments:"))
         for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
-        else:
-            print("{}".format("0 arguments."))
+            print("{}{} {}".format(i, ":", sys.argv[i]))
+    else:
+        print("{}".format("0 arguments."))
