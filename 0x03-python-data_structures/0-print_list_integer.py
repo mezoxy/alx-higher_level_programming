28a144b606b56b72eb2b7c8b6179f36ea9a45fcb
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-09 05:00:13 -0700

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,3 @@
-if __name__ == '__main__':
-    def print_list_integer(my_list=[]):
-        for i in my_list:
-            print("{:d}".format(i))
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
