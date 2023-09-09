6316fc708839a63040b7b47b057fcc091c64aee4
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-09 14:28:24 -0700

    Task7 add conditions

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    b = ()
     if len(tuple_a) > 1:
         if len(tuple_b) > 1:
-            b = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-            return b
+            return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
         elif len(tuple_b) == 1:
-            b = tuple_a[0] + tuple_b[0], tuple_a[1]
-            return b
-        b = tuple_a[0], tuple_a[1]
-        return b
-    else:
-        if len(tuple_b) > 1:
-            return add_tuple(tuple_b, tuple_a)
+            return (tuple_a[0] + tuple_b[0], tuple_a[1])
+        return (tuple_a[0], tuple_a[1])
+    elif len(tuple_b) > 1:
+        return add_tuple(tuple_b, tuple_a)
+    elif len(tuple_b) < 2 and len(tuple_b) < 2:
+        if len(tuple_b) == 1:
+            if len(tuple_a) == 1:
+                return (tuple_a[0] + tuple_b[0], 0)
+            return (tuple_b[0], 0)
         else:
-            return add_tuple(tuple_b, tuple_a)
+            if len(tuple_a) == 1:
+                return (tuple_a[0], 0)
+            return ()
