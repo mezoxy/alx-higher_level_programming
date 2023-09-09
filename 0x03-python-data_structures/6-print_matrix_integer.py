fdb27d0a5c8698155de2f5688ad8cdbfaae6d460
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-09 07:16:44 -0700

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,8 @@
 #!/usr/bin/pytho3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("{}".format(""))
+    for i in matrix:
+        for j in range(len(i)):
+            print("{}".format(int(i[j])), end=" " if j < len(i) - 1 else "")
+        print("{}".format(""))
