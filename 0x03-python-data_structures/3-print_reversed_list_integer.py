f44ec93d69c788211a274839d0b65a51428dc87f
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-10 09:31:01 -0700

    Task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        my_list = []
     my_list.reverse()
     for i in my_list:
         print("{:d}".format(int(i)))
-    if my_list is None:
-        my_list = []
