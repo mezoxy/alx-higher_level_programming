0250538f3687456f5148d03998e58879bfb17ec4
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-10 07:39:38 -0700

    Task 11 changes

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,6 +6,8 @@ def delete_at(my_list=[], idx=0):
         return my_list
     if idx > len(my_list):
         return my_list
+    if len(my_list) == 1:
+        return []
     else:
         my_list.remove(my_list[idx])
         return my_list
