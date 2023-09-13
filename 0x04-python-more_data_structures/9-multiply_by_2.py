cc9fdd84c8d76b6b7d5e54fd0cc0eddd750b4356
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-13 14:44:41 -0700

    Task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    d = {}
     for i in a_dictionary:
-        a_dictionary[i] = a_dictionary.get(i) * 2
-    return a_dictionary
+        d[i] = a_dictionary.get(i) * 2
+    return d
