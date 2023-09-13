0c078b6b7ccbdfa351c17d912c9ad727ff76e3a6
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-13 14:29:37 -0700

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    lis = list(a_dictionary)
-    for i in lis:
-        if i == key:
-            a_dictionary[key] = value
-        a_dictionary[key] = value
+    a_dictionary[key] = value
+    return a_dictionary
