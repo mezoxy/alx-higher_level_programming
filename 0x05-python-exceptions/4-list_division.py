a69c60e4e2733409275f55a729180a3cc766c290
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-25 13:30:11 -0700

    Task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    try:
+        a = [0 for i in range(list_length)]
+        for i in range(list_length):
+            if isinstance(my_list_1[i], (int, float)):
+                if isinstance(my_list_2[i], (int, float)):
+                    if my_list_2[i] != 0:
+                        a[i] = my_list_1[i] / my_list_2[i]
+            else:
+                a[i] = 0
+    except (ZeroDivisionError, IndexError, TypeError):
+        if TypeError:
+            print("wrong type")
+        if ZeroDivisionError:
+            print("division by 0")
+        print("out of range")
+    finally:
+        return a
