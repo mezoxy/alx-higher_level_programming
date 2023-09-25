4faec82f3d5865d49fd52b8d162b188475ebe4bd
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-25 08:10:36 -0700

    Task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    #try:
     j = 0
     for i in range(x):
-        if isinstance(my_list[i], int):
-            print("{:d}".format(my_list[i]), end="")
-            j += 1
+        if not isinstance(my_list[i], int):
             continue
+        print("{:d}".format(my_list[i]), end="")
+        j += 1
     print()
-    #except IndexError as e:
-        #print("{}".format(e))
-    #finally:
+#    except exception:
+#    finally:
     return j
