2bdfd6b90fe5119ee4534f59c8f1561561103856
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-25 07:08:13 -0700

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             y += 1
+        print("")
     except IndexError:
-        print("", end="")
-    print("")
+        print("")
     return y
