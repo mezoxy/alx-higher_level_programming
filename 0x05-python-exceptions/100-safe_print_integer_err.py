f55e3448a44851140b7e25a13fe485a7a7627ee5
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-25 14:49:15 -0700

    Task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except Exception as e:
+    except Exception() as e:
         sys.stderr.write(e)
         return False
     finally:
