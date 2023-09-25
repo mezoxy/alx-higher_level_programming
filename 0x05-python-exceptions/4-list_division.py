d4a73a3c5ceb281e44f6be719d6830772bf4d782
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-25 14:24:54 -0700

    Ts4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -20,5 +20,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
         if IndexError:
             print("out of range")
+    else:
+        return a
     finally:
         return a
