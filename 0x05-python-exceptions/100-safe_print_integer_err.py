974dd5677443a02a02d2490618a475c50b543a46
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-25 15:14:28 -0700

    Task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,10 +5,8 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except Exception() as e:
-        sys.stderr.write(e)
-        return False
-    finally:
-        if isinstance(value, int):
-            return True
+        return True
+    except (ValueError, TypeError):
+        error_message = f"Exception: Cannot print '{value}' as an integer"
+        sys.stderr.write(error_message + "\n")
         return False
