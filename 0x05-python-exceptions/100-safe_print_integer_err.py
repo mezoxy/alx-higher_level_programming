c831e7df69ac246116533e6317c5f267c2d53e23
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-25 15:28:40 -0700

    Task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except Exception as e:
+    except Exception() as e:
         #error = "Exception: Unknown format"
         #message = f"code 'd' for object of type '{type(value).__name__}'"
         print("{}".format(e))
