f7fb6558d4fdccee6edb1c42f61ed24560eb64d1
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-25 13:53:59 -0700

    Task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,8 +2,6 @@
 def list_division(my_list_1, my_list_2, list_length):
     try:
         a = [0 for i in range(list_length)]
-        if not my_list_1 or my_list_2:
-            return a
         for i in range(list_length):
             if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
                 if my_list_2[i] != 0:
