ba05612b395c367a0d5fc80be1ce0a91d1186194
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-26 14:34:22 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Creat a class"""
+
+
+class Square:
+    """A class square.
+    the __init__ function for initialisation"""
+    def __init__(self, size):
+        """Make the size a private attribute"""
+        self.__size = size
