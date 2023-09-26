13eba7aa2cd097861cccc4fbd07ae5387569dd46
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-26 15:40:57 -0700

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Creat a square class"""
 
+
 class Square:
+
     """The __int__ function for initialisation"""
     def __init__(self, size=0):
-        if not  type(size) is int:
+        if not type(size) is int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
