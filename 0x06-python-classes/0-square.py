222ed0bac72de4a95a05de78a2fed99aa695a128
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-26 14:14:20 -0700

    Tadk 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    """An empty class Square that defines a square"""
+    """An empty class Square that defines a square.
+    Pass: To pass the class
+    """
     pass
