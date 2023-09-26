e6aab3f4ff3e9b5c190393121247f599a9e3db7c
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-26 15:55:43 -0700

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A class od square"""
+
+
+class Square:
+    """The __init__ """
+    def __init__(self, size=0):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    """An  instance method that returns he current square area"""
+    def area(self):
+        return self.__size * self.__size
