f2e9791ba70920e321866e666124cd0065e54491
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-02 14:36:23 -0700

    Task

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
-    def __init__(self, width=0, height=0, print_symbol="#"):
+    def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
         self.print_symbol = Rectangle.print_symbol
