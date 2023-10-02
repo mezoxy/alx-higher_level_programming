a959f17c42c94cc2e995bf07cede945ce706d8f4
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-02 14:43:24 -0700

    Task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -52,7 +52,7 @@ class Rectangle:
             return obj
         for i in range(self.__height):
             for j in range(self.__width):
-                obj += self.print_symbol
+                obj += str(self.print_symbol)
             if i == self.__height - 1:
                 obj += ""
             else:
