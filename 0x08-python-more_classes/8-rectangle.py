51cf0fa6a90c621bbcd6ef272795dfdb0b4f28f5
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-02 15:00:52 -0700

    Task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -72,8 +72,9 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not type(rect_2) is Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_2.area() > rect_1.area():
-            return rect_2.area()
-        elif rect_2.area() < rect_1.area():
+        if rect_1.area() > rect_2.area():
             return rect_1.area()
-        return rect_1.area()
+        if rect_2.area() > rect_1.area():
+            return rect_1.area()
+        if rect_1.area() == rect_2.area():
+            return rect_1.area()
