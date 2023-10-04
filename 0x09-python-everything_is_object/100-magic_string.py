f2e2f92ee5942785bd17b94d83f361c3c9cd4b53
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-03 19:06:48 -0700

    task 31

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return "BestSchool" if  magic_string.n == 1 else "BestSchool, BestSchool"
+def magic_string(arr=[]):
+    arr += ["BestSchool"]
+    return (", ".join(arr))
