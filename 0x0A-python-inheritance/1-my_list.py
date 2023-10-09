4436349cc716027d5c94cb3cb5c5a6ef3e4aeb71
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-09 04:36:10 -0700

    Task 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""The module: 1-my_list"""
+
+
+class Mylist(list):
+    """Subclass: Myclist."""
+
+    def print_sorted(self):
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""The module: 2-is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    """is_same_class: A unction that returns True if the object is exactly
+    an instance of the specified class ; otherwise False.
+    Args:
+        obj: The obect to check
+        a_class: The class
+    Return: True if the object is exactly an instance else False
+    """
+    if type(obj) is a_class:
+        return True
+    False
