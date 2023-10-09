80d4798d30ab31cb7ebcdfc12f09bbe2b9fcafbb
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-09 03:46:57 -0700

    Task 0, adding all files

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""The module: 0-lookup"""
+
+
+def lookup(obj):
+    """A function that returns a list of available attributes
+    and methods of an object
+    Arg:
+        obj: An object
+    """
+    return dir(obj)
