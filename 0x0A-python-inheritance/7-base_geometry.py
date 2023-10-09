e0d43c17327a1f757076f013543da35a3fd94061
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-09 16:12:04 -0700

    Task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,20 +3,13 @@
 
 
 class BaseGeometry(list):
-    """A basic class"""
-    def __init__(self, name=None, value=None):
-        self.name = name
-        self.value = value
-
     """An instance method"""
     def area(self):
         raise Exception("area() is not implemented")
 
     """Public method that validate value"""
     def integer_validator(self, name, value):
-        self.name = name
-        self.value = value
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(self.name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(self.name))
+            raise ValueError("{} must be greater than 0".format(name))
