b1f4905592aee288530ecf75665ccfaaf8d6eb41
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-09 12:29:25 -0700

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""The module: 6-base_geometry"""
+
+
+class BaseGeometry(list):
+    """A basic class"""
+    def __init__(self, name=None, value=None):
+        self.name = name
+        self.value = value
+
+    """An instance method"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    """Public method that validate value"""
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(self.name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
