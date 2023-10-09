9269948f6e8c5a3b858449528f215ace1ce36bb7
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-09 05:13:14 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""The module: 6-base_geometry"""
+
+
+class BaseGeometry:
+    """A basic class"""
+    def __init__(self):
+        pass
+
+    """An instance method"""
+    def area(self):
+        raise Exception("area() is not implemented")
