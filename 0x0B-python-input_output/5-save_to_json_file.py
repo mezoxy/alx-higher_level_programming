1280b48b00c564c06eee431e6e7760a29e872737
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-10 05:14:10 -0700

    Task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -13,4 +13,4 @@ def save_to_json_file(my_obj, filename):
         filename: The destination
     """
     with open(filename, 'w') as fl:
-        fl.write(json.dumps(ny_obj))
+        json.dump(ny_obj))
