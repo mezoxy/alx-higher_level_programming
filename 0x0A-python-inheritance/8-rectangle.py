0cdb46c82c14c9622a399fb25cf6d1b03630dcc3
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-09 17:26:28 -0700

    Task 7

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,6 +7,6 @@ class Rectangle(BaseGeometry):
     """New class Rectangle  that inherits from BaseGeometry"""
     def __init__(self, width, height):
         super().integer_validator("width", width)
-        super().integer_validator("height", height)
         self.__width = width
+        super().integer_validator("height", height)
         self.__height = height
