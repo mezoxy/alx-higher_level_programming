bf74109962808984036a0e1af0a63f6bcf1b7d2d
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-10 04:47:16 -0700

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,4 +5,11 @@
 import json
 
 def to_json_string(my_obj):
+    """to_json_string:
+        A function that returns the JSON representation of an object
+    Arg:
+        my_obj: A string
+    Return: 
+        The JSON representation of an object
+    """
     return json.dumps(my_obj)
