a4792d649073c1be1516441a0796f97b03f8d19d
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-10 02:30:07 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """The module: 0-read_file"""
+
+
 def read_file(filename=""):
+    """A function that reads a text file"""
     with open(filename, 'r') as fl:
         print(fl.read())
