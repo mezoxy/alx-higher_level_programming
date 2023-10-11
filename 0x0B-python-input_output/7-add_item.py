48d8ffcc4212939d858ed5659ab36a40debd3a03
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-10 17:33:47 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,12 +3,15 @@
 
 
 from sys import argv
+import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+try:
+    obj = load_from_json_file("add_item.json")
+except Exception:
+    obj = []
 
+obj += argv[1:]
 
-obj1 = argv[1:]
-
-save_to_json_file(obj1, "add_item.json")
-load_from_json_file("add_item.json")
+save_to_json_file(obj, "add_item.json")
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""The module"""
+
+import json
+
+def class_to_json(obj):
+    return json.dumps(obj)
