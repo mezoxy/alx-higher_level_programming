ddadbdc003892a0c439512a0cfd37dc8e9977f49
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-10 19:11:17 -0700

    Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""The module: '9-student'"""
+
+
+class Student:
+    """A class that defines a student by:
+        first_name
+        last_name
+        age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """Public method"""
+    def to_json(self, attrs=None):
+        d = {}
+        if type(attrs) is list:
+            for i in attrs:
+                if hasattr(self, i):
+                    d[i] = getattr(self, i)
+            return d
+        return self.__dict__
+
+    """Public method"""
+    def reload_from_json(self, json):
+        for att in list(json):
+            if hasattr(self, att):
+                self.att = json[att]
