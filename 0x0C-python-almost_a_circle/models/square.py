66bf5de41dc51cae5a34e5f9151a0d6b20b7b859
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-11-03 15:49:29 -0700

    Task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,8 +8,8 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """The subclasse of Rectabgle"""
     def __init__(self, size, x=0, y=0, id=None):
-        self.size = size
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         return "[Square] ({}) {}/{} {}".format(
