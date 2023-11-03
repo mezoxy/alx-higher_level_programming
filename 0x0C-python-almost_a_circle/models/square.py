7a7ffa910832147cce1b2c1b1a9d833ff62adc07
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-11-03 16:22:18 -0700

    Task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,9 +8,25 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """The subclasse of Rectabgle"""
     def __init__(self, size, x=0, y=0, id=None):
-        self.size = size
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
                 self.id, self.x, self.y, self.size)
+
+    """Getter"""
+    @property
+    def size(self):
+        return self.width
+
+    """Setter"""
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("width must be an integer")
+        elif size < 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = size
+            self.height = size
