dedfcfe240bc68a1ed15a5d9f47722673a7fdb67
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-11-03 15:46:42 -0700

    Task 9

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,62 +1,16 @@
 #!/usr/bin/python3
-"""The module : square"""
+"""The module square"""
 
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """The Square class that inherits from the Rectangle class"""
-
+    """The subclasse of Rectabgle"""
     def __init__(self, size, x=0, y=0, id=None):
-        """Constructor"""
-        Rectangle.__init__(self, width=size, height=size, x=x, y=y, id=id)
         self.size = size
+        super().__init__(size, size, x, y, id)
 
-    """Getter and Setter"""
-    @property
-    def size(self):
-        """Getter"""
-        return self.width
-
-    """Set the width an height"""
-    @size.setter
-    def size(self, size):
-        """Setter"""
-        if type(size) is not int:
-            raise TypeError("width must be an integer")
-        if size <= 0:
-            raise ValueError("width must be > 0")
-        self.width = size
-        self.height = size
-
-    """Public Method"""
-    def update(self, *args, **kwargs):
-        """update: Public Method
-        Args:
-            *args and kwargs
-        """
-        lis = ["id", "size", "x", "y"]
-
-        if args:
-            for i in range(min(len(lis), len(args))):
-                if i == 0 and args[0] is None:
-                    self.__init__(self.width, self.height, self.x, self.y)
-                setattr(self, lis[i], args[i])
-        if kwargs:
-            for key in kwargs:
-                if kwargs[key] is None:
-                    self.__init__(self.width, self.height, self.x, self.y)
-                setattr(self, key, kwargs[key])
-
-    """Public method"""
-    def to_dictionary(self):
-        """to_dictionary"""
-        return {'id': self.id, 'size': self.size,
-                'x': self.x, 'y': self.y}
-
-    """The __str__ Method"""
     def __str__(self):
-        """Magic Method"""
-        return "[Square] ({}) {}/{} - {}".format(
+        return "[Square] ({}) {}/{} {}".format(
                 self.id, self.x, self.y, self.size)
