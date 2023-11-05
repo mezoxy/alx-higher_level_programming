1f90b6214653f3d9634da091c06d97f7486771fe
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-11-05 11:38:23 -0800

    Task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,4 +26,5 @@ class Base:
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return dumps(list_dictionaries)
