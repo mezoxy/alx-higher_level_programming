abd6483f678e58de7f51baed218a72a561afc575
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-11-14 12:26:36 -0800

    Task 1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- Write a script that creates the database hbtn_0c_0 in your MySQL server.
-\. hbtn_0c_0.sql
+CREATE database hbtn_0c_0;
--- 0x0D-SQL_introduction/hbtn_0c.sql
+++
@@ -1,2 +0,0 @@
--- bash script to create a database
-SELECT database hbtn_0c_0;
