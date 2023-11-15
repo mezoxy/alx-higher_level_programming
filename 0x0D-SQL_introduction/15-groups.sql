4f299afdb5f3cf0ff13c656d804f8fb65e285081
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-11-14 17:39:57 -0800

    TASK 15 ;)

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0
 SELECT score FROM second_table
-COUNT(score) AS number
+SELECT COUNT(score) AS number second_table
 GROUP BY score ORDER BY score DESC;
