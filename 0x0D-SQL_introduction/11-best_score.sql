0a0d09dd9ba8c1cadc96041e0255e2e311c69c54
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-11-14 16:20:14 -0800

    TASK 11 ;)

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- a script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
-SELECT score, name FROM second_table ORDER BY DESC
+SELECT score, name FROM second_table ORDER BY score DESC
 WHERE score >= 10;
