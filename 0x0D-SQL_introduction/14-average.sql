dc9820b0d68e3af24258ab657b6e5c0a0614c575
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-11-14 17:01:54 -0800

    TASK 14 ;)

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,2 @@
 --  a script that computes the score average of all records in the table second_table of the database hbtn_0c_0
-INSERT INTO second_table average
-VALUES (SELECT AVG(score) FROM second_table);
+SELECT AVG(score) AS averge FROM second_table;
