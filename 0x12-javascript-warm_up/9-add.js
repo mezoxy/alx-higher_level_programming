66b95b1fa8fd7c9d762dfe082710ca6b28988147
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-12-11 15:01:35 -0800

    Task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  if (a == null || b == null) {
+    return ('NaN');
+  } else {
+    return (a + b);
+  }
+}
+
+const c = parseInt(process.argv[2]);
+const d = parseInt(process.argv[3]);
+console.log(add(c, d));
