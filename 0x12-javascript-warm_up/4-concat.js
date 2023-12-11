0eb34fbd84e4aac2c95ec0b91f0fcf67cd3e9995
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-12-11 13:21:03 -0800

    Task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,7 +2,7 @@
 if (process.argv.length === 2) {
   console.log('undefined is undefined');
 } else if (process.argv.length === 3) {
-  console.log(process.argv[2] + ' is ' + 'undefined')
+  console.log(process.argv[2] + ' is ' + 'undefined');
 } else {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
 }
