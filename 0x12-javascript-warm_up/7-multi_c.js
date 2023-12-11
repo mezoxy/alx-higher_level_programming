73d2503402cc1fbcc62438e342a212650c79122d
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-12-11 14:30:04 -0800

    Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of occurrences');
-} else if (parseInt(process.argv[2]) <= 0) {
-  console.log();
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log('C is fun');
