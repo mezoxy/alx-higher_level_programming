daedf5191cef5a066414e20da860e34d32462cf1
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-12-16 12:05:53 -0800

    Task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""module: 9-model_state_filter_a"""
+
+
+import re
+from model_state import Base, State
+from sqlalchemy import create_engine as eng
+import sys
+from sqlalchemy.orm import sessionmaker as se
+
+if __name__ == '__main__':
+
+    conect = eng('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = se(bind=conect)
+    session = Session()
+
+    tabl = session.query(State.id).filter(State.name == sys.argv[4])
+    if tabl:
+        print("{}".format(tabl[0][0]))
+    else:
+        print("Not found")
+
+    session.close()
