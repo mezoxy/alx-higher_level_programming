46614079f5a8767af5ffa6c87d4d8594e422d6d8
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-12-16 10:34:53 -0800

    task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module 7-model_state_fetch_all"""
+
+
+from sqlalchemy import String, Integer, Column, create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3])
+    eng = create_engine(url, echo=False)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+
+    lst = session.query(State).all()
+    print("{}: {}".format(lst[0].id, lst[0].name))
+
+    session.close()
