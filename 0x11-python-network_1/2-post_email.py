20234ad4f7755724b96f63a5d8116538fbda92a5
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-06 18:07:07 -0800

    Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     url = sys.argv[1]
     val = {"email": sys.argv[2]}
 
-    data = urllib.parse.urlencode(val)
+    data = urllib.parse.urlencode(val).encode('utf-8')
     req = urllib.request.Request(url, data)
 
     with urllib.request.urlopen(req) as resp:
