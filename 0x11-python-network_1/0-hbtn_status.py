8e561467fb292f8bde80d4aff7535f8ada110021
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-06 16:18:20 -0800

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,4 @@ with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     print("Body response:")
     print("\t- type:", type(html))
     print("\t- content:", html)
-    if response.headers.get_content_charset() == 'utf-8':
-        print("\t- utf8 content: OK")
-    else:
-        print("\t- utf8 content: NO")
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
