5e9fc53f0dc72e5a2b663dbbd88ce58f70b5a018
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-06 17:52:32 -0800

    Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -15,4 +15,4 @@ if __name__ == '__main__':
     req = urllib.request.Request(url, data)
 
     with urllib.request.urlopen(req) as resp:
-        print(resp.read())
+        print("Your email is:",resp.read())
