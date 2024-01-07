c54f35c9924a68dbb90d5fd72193234bc9104c41
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-07 07:34:42 -0800

    Task 9 Happy conding day

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''The modue: 10-my_github'''
+
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/users/' + sys.argv[1]
+    res = requests.get(url, headers={'password': sys.argv[2]})
+    print(res.json().get('id'))
