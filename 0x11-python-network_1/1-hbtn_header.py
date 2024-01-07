60c287a664a437ed85e5dabdb3d4aabfd2993c44
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-06 16:33:51 -0800

    Task 1 :)

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,8 +2,8 @@
 '''The module: 1-hbtn_header'''
 
 
-import urllib.request as req
+import urllib.request
 import sys
 
-with req.urlopen(sys.argv[1]) as resp:
+with urllib.request.urlopen(sys.argv[1]) as resp:
     print(resp.headers['X-Request-Id'])
