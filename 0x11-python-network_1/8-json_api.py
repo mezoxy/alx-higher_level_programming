40eea5ea8bc828471db0e4a5277987e8d7b19e46
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-07 05:22:31 -0800

    Task 8 HAPPY CODING :)

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/pythin3
+'''The module: 8-json_api'''
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    url = 'http://0.0.0.0:5000/search_user'
+    if sys.argv[1]:
+        res = requests.post(url, date={'q': sys.argv[1]})
+    else:
+        res = requests.post(url, data={'q': ""})
+    try:
+        if len(res.json()) != 0:
+            print("[{}] {}".format(res.json().get('id'), res.json().get('name')))
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")
