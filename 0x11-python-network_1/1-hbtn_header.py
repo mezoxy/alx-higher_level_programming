8ca884f5cd3a61bbc8d1165f0ed59fcf4e0a1617
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-06 16:50:32 -0800

    Task 1 :)

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,5 +5,6 @@
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as resp:
-    print(dict(resp.headers).get('X-Request-Id', None))
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as resp:
+        print(resp.headers.get('X-Request-Id'))
