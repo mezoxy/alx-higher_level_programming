6d4fe9a2d0dbdd226c85114a65aba36e9a9e6c5e
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-06 17:51:25 -0800

    Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''The module: 2-post_email'''
+
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+
+    url = sys.argv[1]
+    val = {"email": sys.argv[2]}
+
+    data = urllib.parse.urlencode(val).encode('utf-8')
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as resp:
+        print(resp.read())
