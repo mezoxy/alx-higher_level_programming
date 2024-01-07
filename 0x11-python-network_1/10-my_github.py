a0d9bbb3913dd945f2f3348dc7adb81b58f2baf8
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-07 07:54:33 -0800

    Task 9 Happy conding day

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,5 +7,5 @@ import requests
 
 if __name__ == '__main__':
     url = 'https://api.github.com/users/' + sys.argv[1]
-    res = requests.get(url, headers={'Authorization': 'token ' +sys.argv[2]})
+    res = requests.get(url, headers={'Authorization': 'token ' + sys.argv[2]})
     print(res.json().get('id'))
