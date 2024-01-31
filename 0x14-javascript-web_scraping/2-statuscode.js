69bb9dce12f8c9e991d37db97f167012876e29dc
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-30 16:53:52 -0800

    task2 Sad night :(

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file
+const request = require('request');
+request(process.argv[2], function (response) {
+  console.log('code:', response.statusCode);
+});
