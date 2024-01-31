c56462284298febbc86e80b2b7cfd5ba64b3e085
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-31 04:33:56 -0800

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,20 +3,23 @@
 const request = require('request');
 const ID = '18'
 request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.log('Error accured');
-  } else {
-    const data = JSON.parse(body);
-    const url = "https://swapi-api.alx-tools.com/api/people/18/";
-    const ur = data.results[0].characters[url];
-    request(ur, function (error, response, body) {
-      if (error) {
-	console.log('Error accured');
-      } else {
-	      const data = JSON.parse(body);
-	      const nmb = data.films.lenght;
-	      console.log(nmb);
-      }
-  });
-  }
+	if (err) {
+		console.log("error");
+	} else {
+		const charc = JSON.parse(body).characters;
+		for (let i = 0; i < charc.lenght; i++) {
+			if (charc[i] == "https://swapi-api.alx-tools.com/api/people/18/") {
+				const request = require('request');
+				request("https://swapi-api.alx-tools.com/api/people/18/"function (error, response, body) {
+					if (err) {
+						console.log("error");
+					} else {
+						const nmb = JSON.parse(body).films.lenght;
+						console.log(nmb);
+						break;
+					}
+				});
+			}
+		}
+	}
 });
