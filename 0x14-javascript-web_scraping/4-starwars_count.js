9e267c1a4ab0932688a6693b520f9cfa817515f9
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-31 12:46:54 -0800

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,20 +2,20 @@
 // script that reads and prints the content of a file
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
-	if (err) {
-		console.log("error");
-	} else {
-		let num = 0;
-		const lis = JSON.parse(body).results;
-		for (let item in lis) {
-			if (lis[item].hasOwnPropert("characters")) {
-				for (let ur in lis[item]["characters"]) {
-					if (lis[item]["characters"][ur] === "https://swapi-api.alx-tools.com/api/people/18/") {
-						num++;
-					}
-				}
-			}
-		}
-		console.log(num);
-	}
+  if (error) {
+    console.log('error');
+  } else {
+    let num = 0;
+    const lis = JSON.parse(body).results;
+    for (const item in lis) {
+      if (lis[item].hasOwnProperty('characters')) {
+        for (const ur in lis[item].characters) {
+          if (lis[item].characters[ur] === 'https://swapi-api.alx-tools.com/api/people/18/') {
+            num++;
+          }
+        }
+      }
+    }
+    console.log(num);
+  }
 });
