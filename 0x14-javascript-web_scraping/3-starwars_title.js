aed2c86d39b29a99c4b131ed866e361d40bcba54
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-30 18:10:19 -0800

    task3 Sad night :(

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 // script that reads and prints the content of a file
 const request = require('request');
-const ur = 'https://swapi-api.alx-tools.com/api/films/'
+const ur = 'https://swapi-api.alx-tools.com/api/films/';
 request(ur + process.argv[2], function (error, response, body) {
   if (error) {
     console.log('Error accured');
   } else {
     const data = JSON.parse(body);
-    
+
     console.log(data.title);
   }
 });
