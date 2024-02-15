48cabfe26833333a2bf40206b482750f9180a003
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-02-15 12:45:25 -0800

    Task 7

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-awk '/^HTT/{print $2}' <(curl -sI "$1")
+awk '/^HTT/{printf $2}' <(curl -sI "$1")
