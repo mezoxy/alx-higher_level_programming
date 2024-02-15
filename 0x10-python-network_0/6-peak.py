d821f6d9ca4488097d21b2e0bff4b22c5a5c23f2
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-02-15 10:11:03 -0800

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""The module: 6-peak"""
+
+
+def find_peak(list_of_integers):
+    """
+        find_peak: A function that finds the biggest num
+                    in a list
+    """
+    if list_of_integers or len(list_of_integers) > 0:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    return None
