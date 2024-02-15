d913b00e185f50326ef550e369886d085059f45f
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-02-15 12:28:39 -0800

    Task 7

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+# A Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
+
+curl -o header.txt -sI "$1" 
+awk '/^HTT/{print $2}' header.txt
---
+++ 0x10-python-network_0/reg.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# Learning regex in bash
+
+string="curl -I $1"
+pettern=''
+
+$string
+# HTTp1.1 343 srgrgsg f
+#if [[ $string != $pattern ]]
+#then
+#	echo "$string"
+#else
+#	echo "No String"
+#fi
